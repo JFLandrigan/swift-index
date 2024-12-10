419394bfabc94b2233c3408316a22fc06c4bd008
jflandrigan <jon.landrigan@gmail.com>
2024-12-09 21:22:35 -0500

    add transform type

--- swift_index/keyword.py
+++ swift_index/keyword.py
@@ -7,18 +7,35 @@ from swift_index.sparse_index import SparseIndex
 SPARSE_TRANSFORMS = ['tfidf', 'count']
 
 class KeyWordIndex(BaseIndex):
+
     def __init__(self):
         self.index: Union[BM25Index, SparseIndex]  = None
+        self.transform_type: str = None
         super().__init__()
 
     def build(self, doc_ids: List[str], docs: List[str], transform: str,):
+
         if transform in SPARSE_TRANSFORMS:
+            self.transform_type = transform
             self.index = SparseIndex()
             self.index.build(doc_ids=doc_ids, docs=docs, transform=transform)
         elif transform == 'bm25':
+            self.transform_type = transform
             self.index = BM25Index()
             self.index.build(doc_ids=doc_ids, docs=docs)
+        else:
+            raise ValueError(f"Expected bm25, tfidf, count, got {transform}")
+
         return
 
-    def search(self, query: Union[str, np.ndarray], num_results: int = 10, return_scores: bool = False,):
-        return self.index.search(query=query, num_results=num_results, return_scores=return_scores)
+    def search(
+        self, 
+        query: Union[str, np.ndarray], 
+        num_results: int = 10, 
+        return_scores: bool = False,
+    ):
+        return self.index.search(
+            query=query, 
+            num_results=num_results, 
+            return_scores=return_scores
+        )
